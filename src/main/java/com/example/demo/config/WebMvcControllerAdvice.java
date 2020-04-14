bebda16d3261736fb18c40512e3a0282a789a4b7
NishiguchiHiroki <nishiguchihiroki2@gmail.com>
2020-04-14 15:38:26 +0900

    first commit

---
+++ src/main/java/com/example/demo/config/WebMvcControllerAdvice.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
+
+
+/**
+ * �S���Controller����������`
+ */
+@ControllerAdvice
+public class WebMvcControllerAdvice {
+	
+//	空で入力された文字をnullに変換する
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        dataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
+   
+}
